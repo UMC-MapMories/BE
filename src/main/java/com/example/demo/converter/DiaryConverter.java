329310894746d1fd7ab9b441ccb4fbf5627cc271
인다운 <incrying@duksung.ac.kr>
2025-02-14 21:41:27 +0900

    Refactor: 위치 저장, 다이어리 작성 기능 통합

--- src/main/java/com/example/demo/converter/DiaryConverter.java
+++ src/main/java/com/example/demo/converter/DiaryConverter.java
@@ -1,23 +1,29 @@
 package com.example.demo.converter;
 
 import com.example.demo.domain.Diary;
+import com.example.demo.domain.User;
 import com.example.demo.dto.DiaryRequestDto;
 import com.example.demo.dto.DiaryResponseDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DiaryConverter {
-    public static Diary toDiary(DiaryRequestDto diaryRequestDto) {
+    public static Diary toEntity(DiaryRequestDto diaryRequestDto, User user) {
         Diary diary = new Diary();
-        diary.setDiaryId(diaryRequestDto.getDiaryId()); // diaryId 포함
-        diary.setCountry(diaryRequestDto.getCountry());
+        diary.setUser(user);
         diary.setDate(diaryRequestDto.getDate());
+        diary.setCountry(diaryRequestDto.getCountry());
         diary.setTitle(diaryRequestDto.getTitle());
         diary.setContent(diaryRequestDto.getContent());
         diary.setImgUrl(diaryRequestDto.getImgUrl());
         diary.setIsOpen(diaryRequestDto.getIsOpen());
         diary.setIsCollaborative(diaryRequestDto.getIsCollaborative());
+        diary.setLatitude(diaryRequestDto.getLatitude());
+        diary.setLongitude(diaryRequestDto.getLongitude());
+        diary.setCreatedAt(LocalDateTime.now());
+        diary.setModifiedAt(LocalDateTime.now());
         return diary;
     }
 
